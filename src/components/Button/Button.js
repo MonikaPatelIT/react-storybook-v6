fb07cdbcfdd2b1ca50339e59901bf3cb3a081f65
Monika Patel <monika.patel@homecaremedical.co.nz>
2021-09-14 21:24:56 +1200

    set up stroybook project with button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Button.css'
+
+function Button({ varient, children, rest }) {
+  return (
+    <button className={`button ${varient}`} {...rest}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
